683547cba071da72b8f78a9841e9cb34f5c50816
Simon Renblad <srenblad@mlabs.hk>
2023-06-13 12:19:58 +0800

    test_scheduler: refactor test_pause, unit feature

--- artiq/test/test_scheduler.py
+++ artiq/test/test_scheduler.py
@@ -288,7 +288,7 @@ class SchedulerCase(unittest.TestCase):
             self.assertEqual(
                 mod,
                 {"action": "setitem", "key": "termination_ok",
-                 "value": (False, True), "path": []})
+                 "value": (False, {'value': True, 'scale': 1.0, 'unit': '', 'ndecimals': 2}), "path": []})
             termination_ok = True
         handlers = {
             "update_dataset": check_termination
