18f8465ab7a849894ef0f6783cc9a341b26c67c8
Simon Renblad <srenblad@m-labs.hk>
2023-06-16 17:20:31 +0800

    channel size feature

--- artiq/dashboard/waveform.py
+++ artiq/dashboard/waveform.py
@@ -18,11 +18,9 @@ class WaveformScene(QtWidgets.QGraphicsScene):
         
         self.channels = []
 
-        self.data = collections.defaultdict(lambda: collections.defaultdict(list))
+        self.data = {"main_channel":  [(10, 0), (0, 10), (14, 20)]}
 
-        self.data = {"main_channel": {0: [(0, 0), (1, 10), (0, 20)], 1: [(0, 0), (1, 20), (0, 40)]}} 
-
-        self.x_scale, self.y_scale, self.row_scale = 100, 10, 2
+        self.x_scale, self.y_scale, self.row_scale = 100, 20, 1.1
         self.x_offset, self.y_offset = 0, 30
 
         self.timescale_unit = "ps"
@@ -44,39 +42,23 @@ class WaveformScene(QtWidgets.QGraphicsScene):
 
         font = QtGui.QFont("Monospace", pointSize=7)
 
-        print(self.timescale)
-        nearest_base = (int(math.log10(self.timescale)) // 3) * 3
-
-        divisor = math.pow(10, nearest_base)
-
-        display_time = int(self.timescale / divisor)
-
         top_band_height = 16
 
         small_tick_height = 8
 
         num_small_tick = 10 # should be factor of x_scale
-        adjusted_start_time = int(self.start_time / divisor)
-        adjusted_end_time = int(self.end_time / divisor)
-        x_start = adjusted_start_time*self.x_scale+self.x_offset
-        x_end = adjusted_end_time*self.x_scale+self.x_offset
+        x_start = self._transform_x(self.start_time)
+        x_end = self._transform_x(self.end_time)
 
         self.addLine(x_start, top_band_height, x_end, top_band_height, pen)
         
-        time = adjusted_start_time
-        for x in range(x_start, x_end, self.x_scale):  
-            for x_sub in range(x, x + self.x_scale, self.x_scale // num_small_tick):
-               self.addLine(x_sub, top_band_height, x_sub, top_band_height + small_tick_height, pen) 
-            self.addLine(x, 0, x, len(self.channels)*self.row_scale*self.y_scale + 100, pen)
-            txt = self.addText(str(time) + " " + self.timescale_unit, font)
-            txt.setPos(x, 0)
+        t = self.start_time
+        while t < self.end_time:
+            self.addLine(self._transform_x(t), 0, self._transform_x(t), len(self.channels)*self.row_scale*self.y_scale + 100, pen)
+            txt = self.addText(str(t) + " " + self.timescale_unit, font)
+            txt.setPos(self._transform_x(t), 0)
             txt.setDefaultTextColor(QtGui.QColor(QtCore.Qt.blue))
-            time += display_time
-        
-        self.addLine(x_end, 0, x_end, len(self.channels)*self.row_scale*self.y_scale + 100, pen)
-        txt = self.addText(str(time) + " " + self.timescale_unit, font)
-        txt.setPos(x_end, 0)
-        txt.setDefaultTextColor(QtGui.QColor(QtCore.Qt.blue))
+            t += self.timescale
     
     # Override
     def wheelEvent(self, event):
@@ -103,41 +85,65 @@ class WaveformScene(QtWidgets.QGraphicsScene):
     def display_graph(self):
         row = 0
         for channel in self.channels:
-            row = self._display_channel(channel, row)
+            row = self._display_channel(channel[0], channel[1:], row)
 
-    def _display_channel(self, channel, row):
-        for address_data in self.data[channel].values():
-            self._display_wire(address_data, row) 
-            row += 1
-        return row
-        
-    def _display_wire(self, address_data, row):
+    def _display_channel(self, channel, subchannels, row):
         pen = QtGui.QPen()
         pen.setStyle(QtCore.Qt.SolidLine)
         pen.setWidth(1)
         pen.setBrush(QtCore.Qt.green)
-        current_value = address_data[0][0]
-        current_stamp = address_data[0][1]
-        for i in range(len(address_data)):
-            if current_value != address_data[i][0]:
-                self.addLine(*self._transform_pos(current_stamp, current_value, address_data[i][1], current_value, row), pen)
-                self.addLine(*self._transform_pos(address_data[i][1], current_value, address_data[i][1], address_data[i][0], row), pen)
-                current_value = address_data[i][0]
-                current_stamp = address_data[i][1]
+
+        sub_pen = QtGui.QPen()
+        sub_pen.setStyle(QtCore.Qt.SolidLine)
+        sub_pen.setWidth(1)
+        sub_pen.setBrush(QtCore.Qt.darkGreen)
+       
+        messages = self.data[channel]
+        current_value = messages[0][0]
+        current_t = messages[0][1]
+        for i in range(len(messages)):
+            new_value = messages[i][0] 
+            new_t = messages[i][1]
+            if current_value != new_value:
+                if current_value > 1:
+                    self.addLine(*self._transform_pos(current_t, 0, new_t, 0, row), pen)
+                    self.addLine(*self._transform_pos(current_t, 1, new_t, 1, row), pen)
+                    self.addLine(*self._transform_pos(new_t, 0, new_t, 1, row), pen)
+                else:
+                    self.addLine(*self._transform_pos(current_t, current_value, new_t, current_value, row), pen)
+                    self.addLine(*self._transform_pos(new_t, current_value, new_t, min(new_value, 1), row), pen)
+                for i, c in enumerate(subchannels):
+                    curr_bit = (current_value >> c) & 1
+                    new_bit = (new_value >> c) & 1
+                    self.addLine(*self._transform_pos(current_t, curr_bit, new_t, curr_bit, row + i + 1), sub_pen)
+                    if new_bit != curr_bit:
+                        self.addLine(*self._transform_pos(new_t, 0, new_t, 1, row + i + 1), sub_pen)
+                current_value = new_value
+                current_t = new_t
+        return row + 1 + len(subchannels)
 
     def _transform_pos(self, x1, y1, x2, y2, row):
-        rx1 = x1*self.x_scale/self.timescale + self.x_offset
-        rx2 = x2*self.x_scale/self.timescale + self.x_offset
-        ry1 = y1*self.y_scale + row*self.row_scale*self.y_scale + self.y_offset
-        ry2 = y2*self.y_scale + row*self.row_scale*self.y_scale + self.y_offset
+        rx1 = self._transform_x(x1)
+        rx2 = self._transform_x(x2)
+        ry1 = self._transform_y(y1) + row * self.row_scale * self.y_scale
+        ry2 = self._transform_y(y2) + row * self.row_scale * self.y_scale
         return (rx1, ry1, rx2, ry2)
+
+    def _transform_x(self, x):
+        return x * self.x_scale / self.timescale + self.x_offset
+
+    def _transform_y(self, y):
+        return y * self.y_scale + self.y_offset
 
     def increase_timescale(self):
         self.timescale *= 5
+        self.x_offset = self.x_offset // 5
         self.refresh_display()
 
     def decrease_timescale(self):
-        self.timescale = max(int(self.timescale / 5), 1)
+        if not self.timescale < 5:
+            self.timescale = self.timescale // 5
+            self.x_offset = self.x_offset * 5
         self.refresh_display()
 
 class WaveformChannelList(QtWidgets.QListWidget):
@@ -146,7 +152,7 @@ class WaveformChannelList(QtWidgets.QListWidget):
     def __init__(self):
         QtWidgets.QListWidget.__init__(self)
 
-        self.channels = ["main_channel"]
+        self.channels = {"main_channel": [0, 1]}
         for channel in self.channels:
             self.addItem(channel)
 
@@ -178,14 +184,15 @@ class AddChannelDialog(QtWidgets.QDialog):
         self.close()
 
 class WaveformActiveChannelView(QtWidgets.QTreeWidget):
-    update_channels_signal = QtCore.pyqtSignal(list)
+    update_active_channels_signal = QtCore.pyqtSignal(list)
 
     def __init__(self):
         QtWidgets.QTreeView.__init__(self)
         self.active_channels = []
-        self.channels = []
+        self.channels = {"main_channel": [0, 1]}
         self.setMaximumWidth(150)
         self.setHeaderLabel("Channels")
+        self.setIndentation(5)
         
         self.setContextMenuPolicy(QtCore.Qt.ActionsContextMenu)
         add_channel = QtWidgets.QAction("Add channel", self)
@@ -195,20 +202,22 @@ class WaveformActiveChannelView(QtWidgets.QTreeWidget):
         self.addAction(add_channel)
 
         self.add_channel_dialog = AddChannelDialog(self)
-        self.add_channel_dialog.add_channel_signal.connect(self.add_channel)
-
+        self.add_channel_dialog.add_channel_signal.connect(self.add_active_channel)
 
     def add_channel_widget(self):
         self.add_channel_dialog.open()
 
-    def add_channel(self, name):
+    def add_active_channel(self, name):
         item = QtWidgets.QTreeWidgetItem([name])
-        self.channels.append(name)
+        self.active_channels.append([name] + [x for x in self.channels[name]])
+        for sbc in self.channels[name]:
+            child = QtWidgets.QTreeWidgetItem([name + "[" + str(sbc) + "]"])
+            item.addChild(child)
         self.addTopLevelItem(item)
-        self.update_channels()
+        self.update_active_channels()
 
-    def update_channels(self):
-        self.update_channels_signal.emit(self.channels)
+    def update_active_channels(self):
+        self.update_active_channels_signal.emit(self.active_channels)
 
 
 class WaveformDock(QtWidgets.QDockWidget):
@@ -221,10 +230,10 @@ class WaveformDock(QtWidgets.QDockWidget):
         grid = LayoutWidget()
         self.setWidget(grid)
 
-        self.zoom_in_button = QtWidgets.QPushButton()
+        self.zoom_in_button = QtWidgets.QPushButton("Zoom In")
         grid.addWidget(self.zoom_in_button, 0, 0)
 
-        self.zoom_out_button = QtWidgets.QPushButton()
+        self.zoom_out_button = QtWidgets.QPushButton("Zoom Out")
         grid.addWidget(self.zoom_out_button, 0, 1)
 
 
@@ -237,7 +246,7 @@ class WaveformDock(QtWidgets.QDockWidget):
 
         self.waveform_view.setAlignment(QtCore.Qt.AlignTop | QtCore.Qt.AlignLeft)
 
-        self.waveform_active_channel_view.update_channels_signal.connect(self.waveform_scene.update_channels)
+        self.waveform_active_channel_view.update_active_channels_signal.connect(self.waveform_scene.update_channels)
         
         self.zoom_in_button.clicked.connect(self.waveform_scene.decrease_timescale)
         self.zoom_out_button.clicked.connect(self.waveform_scene.increase_timescale)
