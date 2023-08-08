573d521feb7ca780a6d39995f13f6110f71f2c2f
Simon Renblad <srenblad@m-labs.hk>
2023-08-08 17:39:56 +0800

    artiq_rtio_proxy: remove hello world

--- artiq/frontend/artiq_rtio_proxy.py
+++ artiq/frontend/artiq_rtio_proxy.py
@@ -28,7 +28,7 @@ class DummyControl:
         self.notifier = Notifier(self.data)
 
     def pull_from_device(self):
-        dump = b"Hello World"
+        dump = b""
         with open("dump7.bin", "rb") as f:
             dump = f.read()
         self.notifier["data"] = dump
